b340af6888c2352bd8b65894a218bf7c6864b25c
derickmpark@gmail.com <derickmpark@gmail.com>
2023-02-03 17:31:39 -0800

    add: About

---
+++ src/components/About.tsx
@@ -0,0 +1,48 @@
+import { Flex, Image, Grid } from "@chakra-ui/react";
+import { SFMonoPointer } from "src/components/shared/Pointer";
+import SectionHeader from "src/components/shared/SectionHeader";
+import { NAV_ABOUT_ID } from "./NavBar";
+import SectionContainer from "src/components/shared/SectionContainer";
+import { Text } from "src/general/Text";
+
+export default function About(): JSX.Element {
+  return (
+    <SectionContainer id={NAV_ABOUT_ID}>
+      <SectionHeader number="01." title={`About Me`} />
+      <Grid
+        color="slate.300"
+        justifyItems="center"
+        gridTemplateColumns={{ base: "1fr", md: "2fr 1fr" }}
+        gap={10}
+      >
+        <Flex flexDirection="column">
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <Grid gridTemplateColumns="repeat(2, 1fr)">
+            <SFMonoPointer size="sm">Typescript</SFMonoPointer>
+            <SFMonoPointer>GraphQL</SFMonoPointer>
+            <SFMonoPointer>NextJS</SFMonoPointer>
+            <SFMonoPointer>ReduxJS</SFMonoPointer>
+            <SFMonoPointer>React Query</SFMonoPointer>
+            <SFMonoPointer>Chakra UI</SFMonoPointer>
+          </Grid>
+        </Flex>
+        <Image
+          alt="Portrait"
+          src="/portrait.jpeg"
+          minW={300}
+          minH={300}
+          w={300}
+          h={300}
+        />
+      </Grid>
+    </SectionContainer>
+  );
+}
